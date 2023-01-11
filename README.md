f6a4a896944675c5ebe3541fda5184949fb6c72a
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2023-01-11 18:54:59 -0300

    feat: add docs and prepare to deploy :sparkles:

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![CI](https://github.com/zoedsoupe/lucide_icons/actions/workflows/ci.yml/badge.svg)
 
-This package adds a convenient way of using [Lucide](https://lucide.dev/) with your Phoenix and Surface applications.
+This package adds a convenient way of using [Lucide](https://lucide.dev/) with your Phoenix and Phoenix.LiveView applications.
 
 Lucide is "An open source icon library for displaying icons and symbols in digital and non-digital projects. It consists of 850+ Vector (svg) files", and is a fork of Feather Icons.
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Lucideicons.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_url "https://github.com/zoedsoupe/lucide_icons"
+
   def project do
     [
       app: :lucide_icons,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
+      description: description(),
+      package: package(),
+      source_url: @github_url
     ]
   end
 
@@ -17,11 +24,42 @@ defmodule Lucideicons.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix_html, "~> 3.2"},
       {:phoenix_live_view, "~> 0.17", optional: true},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      main: "Lucideicons",
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      groups_for_modules: [LiveView: ~r/Lucideicons.LiveView/],
+      nest_modules_by_prefix: [Lucideicons.LiveView],
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description do
+    """
+    This package adds a convenient way of using Lucide
+    with your Phoenix and Phoenix LiveView applications.
+
+    Lucide is "An open source icon library for displaying
+    icons and symbols in digital and non-digital projects.
+    It consists of 850+ Vector (svg) files", and is a fork
+    of Feather Icons.
+    """
+  end
+
+  defp package do
+    %{
+      files: ~w(lib priv .formatter.exs mix.exs README.md LICENSE),
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => @github_url}
+    }
   end
 end
